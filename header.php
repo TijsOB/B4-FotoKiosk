2a7bc38663b9c21b745ecb0be4444cef1664ffb3
TijsOB <tijsblokje@gmail.com>
2026-05-12 11:26:27 +0200

    header.php base

---
+++ header.php
@@ -0,0 +1,6 @@
+<header>
+    <div class="header-content">
+        <div class="wrapper">
+        <img src="<?php echo $base_url; ?>/images/logo-big-v3.png" alt="Het logo van DeveloperLand met een draaimolen, kasteel, achtbaan en tot slot een gezin op de voorgrond." class="logo hidden-on-sm">
+        </div>
+</header>       
