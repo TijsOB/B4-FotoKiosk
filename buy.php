f60c1e1fd9b118bc59860a9a2215bafae55aea54
RobinCuy <cuyckensinnederland@gmail.com>
2026-05-28 09:49:28 +0200

    basis shopping page

--- buy.php
+++ buy.php
@@ -3,9 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Shopping Cart</title>
 </head>
 <body>
-    
+    <header>
+        <div class="header-content">
+            <div class="wrapper">   
+            <img src="/pictures/img/logo-big-v3.png" alt="Het logo van DeveloperLand met een draaimolen, kasteel, achtbaan en tot slot een gezin op de voorgrond." class="logo hidden-on-sm">
+            </div>
+        </div>       
+    </header>  
+
+    <main>
+        <div class="nav">
+                <a href="index.php">&larr; Terug</a>
+        </div>
+
+        <?php $placeholder = 0 ?>
+        <?php $photos[] = 1 ?>
+        
+        <?php if(!empty($photos)) ?>
+            <?php foreach ($photos as $photo): ?>
+                <?php $placeholder += 500 ?>
+                <h2>Totaal prijs = €<?php echo $placeholder ?></h2>
+            <?php endforeach; ?>
+    </main>
 </body>
 </html>
--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <body>
    <header>
     <div class="header-content">
-        <div class="wrapper">
+        <div class="wrapper">   
         <img src="/pictures/img/logo-big-v3.png" alt="Het logo van DeveloperLand met een draaimolen, kasteel, achtbaan en tot slot een gezin op de voorgrond." class="logo hidden-on-sm">
         </div>
 </div>
