60daee0f2addf171f0825dbd1752de1807398d83
TijsOB <tijsblokje@gmail.com>
2026-06-04 08:43:41 +0200

    `day row class toegevoegd

--- index.php
+++ index.php
@@ -33,26 +33,31 @@ session_start();
             </div>
 
         <div class="container">
-        <div class="day">
-            <a href="days.php?day=Zondag"><h2>Zondag</h2></a>
-        </div> 
-        <div class="day">
-            <a href="days.php?day=Maandag"><h2>Maandag</h2></a>
-        </div>
-        <div class="day">
-            <a href="days.php?day=Dinsdag"><h2>Dinsdag</h2></a>
-        </div>
-        <div class="day">
-            <a href="days.php?day=Woensdag"><h2>Woensdag</h2></a>
-        </div>
-        <div class="day">
-            <a href="days.php?day=Donderdag"><h2>Donderdag</h2></a>
-        </div>
-        <div class="day">
-            <a href="days.php?day=Vrijdag"><h2>Vrijdag</h2></a>
-        </div>
-        <div class="day">
-            <a href="days.php?day=Zaterdag"><h2>Zaterdag</h2></a>
+            <div class="days-row days-row--four">
+                <div class="day">
+                    <a href="days.php?day=Zondag"><h2>Zondag</h2></a>
+                </div>
+                <div class="day">
+                    <a href="days.php?day=Maandag"><h2>Maandag</h2></a>
+                </div>
+                <div class="day">
+                    <a href="days.php?day=Dinsdag"><h2>Dinsdag</h2></a>
+                </div>
+                <div class="day">
+                    <a href="days.php?day=Woensdag"><h2>Woensdag</h2></a>
+                </div>
+            </div>
+            <div class="days-row days-row--three">
+                <div class="day">
+                    <a href="days.php?day=Donderdag"><h2>Donderdag</h2></a>
+                </div>
+                <div class="day">
+                    <a href="days.php?day=Vrijdag"><h2>Vrijdag</h2></a>
+                </div>
+                <div class="day">
+                    <a href="days.php?day=Zaterdag"><h2>Zaterdag</h2></a>
+                </div>
+            </div>
         </div>
     </main>
 </body>
