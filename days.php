a4a7c2eed7d72972fb27f0b8f2359b1ccfe1e8bb
TijsOB <tijsblokje@gmail.com>
2026-06-04 10:04:45 +0200

    filter toegevoegd

--- days.php
+++ days.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 
+include 'helpers.php';
+
 $dayRaw = filter_input(INPUT_GET, 'day');
 $day = $dayRaw ? trim($dayRaw) : '';
 
@@ -21,20 +23,37 @@ $dayMap = [
 
 $folderName = $dayMap[$day] ?? null;
 $photos = [];
+$filterHourRaw = filter_input(INPUT_GET, 'hour');
+$filterMinuteRaw = filter_input(INPUT_GET, 'minute');
+$filterHour = ($filterHourRaw !== null && $filterHourRaw !== '') ? filter_var($filterHourRaw, FILTER_VALIDATE_INT, ['options' => ['min_range' => 0, 'max_range' => 23]]) : null;
+$filterMinute = ($filterMinuteRaw !== null && $filterMinuteRaw !== '') ? filter_var($filterMinuteRaw, FILTER_VALIDATE_INT, ['options' => ['min_range' => 0, 'max_range' => 59]]) : null;
+if ($filterHour === false) {
+    $filterHour = null;
+}
+if ($filterMinute === false) {
+    $filterMinute = null;
+}
 
+$availableHours = [];
 if ($folderName) {
     $folderPath = 'pictures/' . $folderName;
-    if (is_dir($folderPath)) {
-        $files = scandir($folderPath);
-        $imageExtensions = ['jpg', 'jpeg', 'png', 'gif', 'webp'];
-        foreach ($files as $file) {
-            $ext = strtolower(pathinfo($file, PATHINFO_EXTENSION));
-            if (in_array($ext, $imageExtensions)) {
-                $photos[] = $file;
-            }
-        }
-        sort($photos);
+    $photos = loadPhotosFromFolder($folderPath);
+    $availableHours = getPhotoHours($photos);
+    $photos = filterPhotosByTime($photos, $filterHour, $filterMinute);
+}
+
+function makeQuery(array $extra = []): string
+{
+    global $day, $filterHour, $filterMinute;
+    $params = ['day' => $day];
+    if ($filterHour !== null) {
+        $params['hour'] = $filterHour;
     }
+    if ($filterMinute !== null) {
+        $params['minute'] = $filterMinute;
+    }
+
+    return http_build_query(array_merge($params, $extra));
 }
 
 $pageTitle = $day;
@@ -60,11 +79,31 @@ include 'partials/header.php';
             </div>
         </header>
 
+        <div class="sort-toolbar">
+            <form method="get" class="sort-toolbar__form">
+                <input type="hidden" name="day" value="<?php echo htmlspecialchars($day, ENT_QUOTES, 'UTF-8'); ?>">
+
+                <label for="hour">Uur</label>
+                <select name="hour" id="hour">
+                    <option value="">Alle uren</option>
+                    <?php foreach ($availableHours as $hourOption): ?>
+                        <option value="<?php echo $hourOption; ?>"<?php echo $filterHour === $hourOption ? ' selected' : ''; ?>><?php echo str_pad($hourOption, 2, '0', STR_PAD_LEFT); ?>:00</option>
+                    <?php endforeach; ?>
+                </select>
+
+                <label for="minute">Minuut</label>
+                <input type="number" name="minute" id="minute" min="0" max="59" step="1" value="<?php echo $filterMinute !== null ? htmlspecialchars($filterMinute, ENT_QUOTES, 'UTF-8') : ''; ?>" placeholder="0">
+
+                <button type="submit" class="btn btn--secondary">Filteren</button>
+                <a href="days.php?day=<?php echo urlencode($day); ?>" class="btn btn--tertiary"><button class="btn btn--secondary">Reset</button></a>
+            </form>
+        </div>
+
         <?php if (!empty($photos)): ?>
             <div class="photo-grid">
                 <?php $id = 0; foreach ($photos as $photo): $id++; ?>
                     <article class="photo-card">
-                        <a class="photo-card__media" href="photo.php?day=<?php echo urlencode($day); ?>&id=<?php echo $id; ?>">
+                        <a class="photo-card__media" href="photo.php?<?php echo makeQuery(['id' => $id]); ?>">
                             <img loading="lazy"
                                  src="pictures/<?php echo htmlspecialchars($folderName, ENT_QUOTES, 'UTF-8'); ?>/<?php echo htmlspecialchars($photo, ENT_QUOTES, 'UTF-8'); ?>"
                                  alt="<?php echo htmlspecialchars($day, ENT_QUOTES, 'UTF-8'); ?> foto <?php echo $id; ?>">
@@ -76,7 +115,7 @@ include 'partials/header.php';
                                 <span class="photo-card__price">&euro;12,50</span>
                             </div>
                             <a class="btn btn--primary btn--block"
-                               href="photo.php?day=<?php echo urlencode($day); ?>&id=<?php echo $id; ?>">
+                               href="photo.php?<?php echo makeQuery(['id' => $id]); ?>">
                                 <i class="fa-solid fa-cart-shopping"></i> Bekijk &amp; koop
                             </a>
                         </div>
